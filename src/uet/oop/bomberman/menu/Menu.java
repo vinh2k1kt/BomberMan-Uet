6d624e4e3ad8b6c652a5dd4fb3bb899d615178f3
kwanuwu <nguyenvietquan2501@gmail.com>
2021-12-06 22:45:53 +0700

    gameOver scene + image

--- src/uet/oop/bomberman/menu/Menu.java
+++ src/uet/oop/bomberman/menu/Menu.java
@@ -6,33 +6,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import uet.oop.bomberman.Level;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
 public class Menu {
 
-    public Stage primaryStage;
+    Stage primaryStage;
+
+    public static Level level;
 
     @FXML
-    Button buttonPlay = new Button();
-    @FXML
-    Button buttonExit = new Button();
-
+    AnchorPane anchorPane = new AnchorPane();
     public void playButton(ActionEvent event) throws IOException {
-
-//        primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        LoadingScene loadingScene = new LoadingScene(primaryStage, "res/levels/Level1.txt");
-//
-//        primaryStage.setScene(loadingScene.getScene());
-        System.out.println("Clicked");
+        level.pause();
     }
 
     public void exitButton(ActionEvent event) throws IOException {
@@ -43,9 +34,10 @@ public class Menu {
     public void howtoplayButton(ActionEvent event) throws IOException {
         URL url = new File("src/uet/oop/bomberman/menu/howtoplay.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
-        root.getStylesheets().add("uet/oop/bomberman/menu/image/style.css");
         primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         primaryStage.setTitle("BomberMan");
         primaryStage.setScene(new Scene(root));
+        root.getStylesheets().add("uet/oop/bomberman/menu/image/style.css");
     }
 }
+
